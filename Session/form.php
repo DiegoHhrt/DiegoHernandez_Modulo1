c5a093c249135d1c7ab53f29259817ed487cf4e9
Diego Hernandez <redbandito@outlook.com>
2021-05-06 22:54:01 -0500

    Feat: Session info saved

--- Session/form.php
+++ Session/form.php
@@ -1,47 +1,52 @@
-<!DOCTYPE html>
-<html>
-    <?php
-        if((isset($_SESSION["start"])))
-        {
-            header("location: ./index.php");
-        }
-    ?>
-    <head>
-        <meta charset="UTF-8">
-        <title>Inicio de sesión</title>
-    </head>
-    <body>
-        <form action="./index.php" method="post">
-        <fieldset>
-            <legend>Inicio de sesión</legend>
-            <label for="Name">
-                Nombre:  <input type="text" name="Name" placeholder="Nombre(s)" required>
-            </label>
-            <br><br>
-            <label for="Apellido">
-                Apellido: <input type="text" name="Apellido" placeholder="Apellido(s)" required>
-            </label>
-            <br><br>
-            <label for="Group">
-                Grupo: <input type="text" name="Group" placeholder="Grupo" required>
-            </label>
-            <br><br>
-            <label for="birth">
-            <?php
-                echo "Fecha de nacimiento:  <input type='date' name='birth' min='1930-01-01' max=".date("Y-m-d")." required>"; 
-                var_dump($_SESSION['start']);
-            ?>
-            </label>
-            <br><br>
-            <label for="mail">
-                Correo electrónico: <input type="email" name="mail" placeholder="example@mail.com" required>
-            </label>
-            <br><br>
-            <label for="passwd">
-                Contraseña: <input type="password" name="passwd" required>
-            </label>
-            <button type="submit" name="send" value="init">Iniciar sesión</button>
-        </fieldset>
-        </form>
-    </body>
-</html>
+<?php
+
+if((isset($_SESSION['start'])))
+{
+    header('location: ./index.php');
+}
+else
+{
+    echo"
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset='UTF-8'>
+            <title>Inicio de sesión</title>
+        </head>
+        <body>
+            <form action='./index.php' method='post'>
+            <fieldset>
+                <legend>Inicio de sesión</legend>
+                <label for='Name'>
+                    Nombre:  <input type='text' name='Name' placeholder='Nombre(s)' required>
+                </label>
+                <br><br>
+                <label for='Apellido'>
+                    Apellido: <input type='text' name='Apellido' placeholder='Apellido(s)' required>
+                </label>
+                <br><br>
+                <label for='Group'>
+                    Grupo: <input type='text' name='Group' placeholder='Grupo' required>
+                </label>
+                <br><br>
+                <label for='birth'>";
+                
+                    echo "Fecha de nacimiento:  <input type='date' name='birth' min='1930-01-01' max='.date('Y-m-d').' required>"; 
+                    //var_dump($_SESSION['start']);
+                echo "
+                </label>
+                <br><br>
+                <label for='mail'>
+                    Correo electrónico: <input type='email' name='mail' placeholder='example@mail.com' required>
+                </label>
+                <br><br>
+                <label for='passwd'>
+                    Contraseña: <input type='password' name='passwd' required>
+                </label>
+                <button type='submit' name='send' value='init'>Iniciar sesión</button>
+            </fieldset>
+            </form>
+        </body>
+    </html>";
+}
+?>
--- Session/index.php
+++ Session/index.php
@@ -1,8 +1,16 @@
 <?php
-
+    session_name("DDDDD");
+    session_start();
+    
     $button=(isset ($_POST["send"]) && $_POST["send"] !="") ?$_POST["send"]: false;
 
-if($button||(isset($_SESSION["start"])))
+if(isset($_POST["exit"]))
+{
+    session_unset();
+    session_destroy();
+    header("location: ./form.php");
+}
+elseif($button||(isset($_SESSION["start"])))
 {
     if($button)
     {
@@ -12,9 +20,6 @@ if($button||(isset($_SESSION["start"])))
         $birth=(isset($_POST["birth"]) && $_POST["birth"] != "")? $_POST["birth"]:"Inválido";
         $mail=(isset($_POST["mail"]) && $_POST["mail"] != "")? $_POST["mail"]:"Inválido";
         $pw=(isset($_POST["passwd"]) && $_POST["passwd"] != "")? $_POST["passwd"]:"Inválido";
-        
-        session_name("DDDDD");
-        session_start();
 
         $_SESSION['start']=true;
         $_SESSION['nombre']=$name;
@@ -28,7 +33,32 @@ if($button||(isset($_SESSION["start"])))
     }
     if((isset($_SESSION["start"])))
     {
-        echo "ola";
+        echo "
+        <table border='1'>
+            <thead>
+                <tr>
+                    <th colspan='2'>Información de inicio de sesión</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Nombre completo:</td>
+                    <td>".$_SESSION['nombre']." ".$_SESSION['apellido']."</td>
+                </tr>
+                <tr>
+                    <td>Grupo:</td>
+                    <td>".$_SESSION['grupo']."</td>
+                </tr>
+                <tr>
+                    <td>Fecha de nacimiento:</td>
+                    <td>".$_SESSION['cumple']."</td>
+                </tr>
+                <tr>
+                    <td>Correo electrónico:</td>
+                    <td>".$_SESSION['email']."</td>
+                </tr>
+            </tbody>
+        </table>";
     }
 
     echo "<br><br><form action='./index.php' method='POST' >
@@ -39,12 +69,5 @@ elseif(!(isset($_SESSION["start"])))
 {
     header("location: ./form.php");
 }
-elseif(isset($_POST["exit"]))
-{
-    session_unset();
-    session_destroy();
-    header("location: ./form.php");
-}
-
 
 ?>
